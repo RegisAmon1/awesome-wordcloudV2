687441afa86b9a18c20e6d2be75cf9af99d8e252
RegisAmon1 <116574013+RegisAmon1@users.noreply.github.com>
2022-11-17 08:31:47 +0100

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -278,9 +278,9 @@ def main():
     background_color=st.sidebar.selectbox("Background Color ?",["white","black"])
     #background_color = st.sidebar.color_picker("Background color")
     icon = st.sidebar.selectbox("Icon ?",["flag","at","cloud","smile","thumbs-up","user","bolt","angle-right","angle-double-right","mask","calendar-check"
-                                               ,"balance-scale","battery-full","bell","birthday-cake","search","tag","dog","barcode","user-secret","shield-halver","shield-check","shield","binary-lock",
+                                               ,"balance-scale","battery-full","bell","birthday-cake","search","tag","dog","barcode","user-secret","shield-halved","shield-check","shield",
                                                "book-reader","chart-line","coffee","comment","comments","tshirt","bullseye","clinic-medical","house-user","hospital-alt"
-                                               ,"cube","cubes","expand-arrows-alt","mug-hot","bullhorn","egg","envelope","lock","user-lock","lock-hashtag","building-lock","key","hand-spock",
+                                               ,"cube","cubes","expand-arrows-alt","mug-hot","bullhorn","egg","envelope","lock","user-lock","building-lock","key","hand-spock",
                                                "glass-martini","globe-africa","grin","grin-hearts","graduation-cap","laugh"
                                                ,"microphone","paper-plane","lock-open","percent","hand-holding-medical","people-carry"
                                                 ,"hand-holding-heart","comment-medical","file-medical","heart","heartbeat","brain","book-medical","laptop-medical"])
